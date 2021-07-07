949ccce259c5957b9dfef218b5a0ec5d10673bd9
Radonevsky <Radonevsky@users.noreply.github.com>
2021-07-07 20:35:33 +0300

    Added slick slider of background images for the intro block

--- js/app.js
+++ js/app.js
@@ -121,5 +121,35 @@ $(function () {
     }, 200);
   }
 
+  
+  /* Slick slider https://kenwheeler.github.io/slick/
+  ================================================================*/
+let introSlider = $('#introSlider');
+
+introSlider.slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    speed: 500
+  });
+
+$('#introSliderPrev').on('click', function() {
+  introSlider.slick('slickPrev')
+});
+
+$('#introSliderNext').on('click', function() {
+  introSlider.slick('slickNext')
+});
+
+
+
+
 
 });
+
+
+
